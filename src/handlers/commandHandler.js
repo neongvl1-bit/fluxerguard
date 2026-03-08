313ac703b874cb47f433136e47653d58cedfef04
neongvl1-bit <neongvl1@gmail.com>
2026-03-08 02:32:05 +0200

    upload src/handlers/commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -199,9 +199,6 @@ async function handleMessage(api, message) {
     }
   }
 
-  const { trackCommand } = require('../utils/dna');
-  trackCommand(message.guild_id, cmdName).catch(() => {});
-
   try {
     await cmd.execute({
       api,
